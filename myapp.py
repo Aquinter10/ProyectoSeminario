3e00a24448f75a7890ac49b765854e659cbcf808
Aquinter10 <ndroxgoodplayer@gmail.com>
2024-05-16 18:50:49 -0500

    Prueba
    Commit

--- myapp.py
+++ myapp.py
@@ -69,3 +69,33 @@ plt.xticks(rotation=45)
 # Mostrar la gráfica en Streamlit
 st.title('Análisis de Tipos de Seguro')
 st.pyplot(plt)
+
+
+st.write("""
+         En este grafico de barras se evidencia la distribucion de clientes por cada tipo de seguros. Sin embargo, para el modo en que se tomaron los datos la compania uso una fuente de datos diferente a la original.
+         
+         Por lo tanto, se decide revisar estos datos y compararlos con los datos fuente.
+
+         Desde el Dataframe usado para la grafica se pueden encontrar algunas inconsistencias, como el caso de datos duplicados.
+         
+         """)
+
+st.dataframe(dpf)
+
+
+st.write("---")
+
+
+st.title('Comparación de DataFrames')
+col1, col2 = st.columns(2)
+with col1:
+    st.header("DataFrame de Clientes mas reciente")
+    st.dataframe(counts)
+with col2:
+    st.header("DataFrame Original Clientes")
+    st.dataframe(conteo_tipos_seguro)
+
+
+st.write("Dada la inconsistencia de ambos datos se determina que hubo error a la hora de la migracion de datos. Puntualmente hubo una neglicencia en transferir datos generales de clientes para determinar la proporcion de seguros y hacerlo manualmente. De haber sido el caso, esto explicaria los datos duplicados.")
+
+st.write()
